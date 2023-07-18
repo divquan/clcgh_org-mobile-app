4f8d1774e24e5d78ce625907edf4f25bf6c57944
Divine Quansah <divquan@gmail.com>
2023-07-18 23:22:33 +0000

    completed splash screen

--- lib/splash.dart
+++ lib/splash.dart
@@ -1,7 +1,28 @@
+import 'package:clcgh_org/main.dart';
 import 'package:flutter/material.dart';
 
-class Splash extends StatelessWidget {
+class Splash extends StatefulWidget {
   const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToHome();
+  }
+
+  void _navigateToHome() async {
+    await Future.delayed(const Duration(milliseconds: 1500), () {});
+    // ignore: use_build_context_synchronously
+    Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+            builder: ((context) => const MyHomePage(title: 'clcgh.org'))));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,13 +34,13 @@ class Splash extends StatelessWidget {
         child: isDarkMode
             ? Image.asset(
                 'assets/images/logo_white_H.png',
-                width: 170,
-                height: 170,
+                width: 140,
+                height: 140,
               )
             : Image.asset(
                 'assets/images/logo_blue.png',
-                width: 170,
-                height: 170,
+                width: 140,
+                height: 140,
               ),
       ),
     );
